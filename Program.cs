75485044d51fd996dc93118bcb7afb8788faae75
WindingTheRopes <ilikecomputers7@gmail.com>
2019-12-05 17:15:28 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Flex_Shell
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("FLEX SHELL VERSION 0.0.4");
+            Console.WriteLine("Created by Jack Anderson");
+            Console.WriteLine("Go to https://www.github.com/WindingTheRopes/flex");
+            Console.WriteLine();
+            fMain();
+        }
+        public static void fMain()
+        {
+            Console.Write("FLEX$> ");
+            string fCMD = Console.ReadLine();
+            fMain();
         }
     }
 }
