b7f5a9ca25e9b105bff6d2bbd3c1e6d9a69a1785
WindingTheRopes <ilikecomputers7@gmail.com>
2019-12-05 20:11:50 -0500

    add globals modify program
    add globals for global variables, modify program to use global variable fcmd, cache is where the cache will be stored.

--- Program.cs
+++ Program.cs
@@ -4,19 +4,23 @@ using System.IO;
 
 namespace Flex_Shell
 {
+   
     public class Program
     {
 
-        public void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("FLEX SHELL VERSION 0.0.4");
             Console.WriteLine("Created by Jack Anderson");
             Console.WriteLine("Go to https://www.github.com/WindingTheRopes/flex");
             Console.WriteLine();
-            Console.Write("FLEX$> ");
-            string fCMD = Console.ReadLine();
-
-
         }
+        void flex()
+        {
+            Console.Write("FLEX$> ");
+            globals.FCMD = Console.ReadLine();
+            
+        }
+            
     }
 }
---
+++ globals.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public static class globals
+{
+    public static String CACHE = "flex-cache.txt";
+    public static String FCMD = null;
+}
+
