711c7505a1fc73dfdcba797032bf72d1b5c9fd87
WindingTheRopes <ilikecomputers7@gmail.com>
2019-12-05 19:40:01 -0500

    Create Math.cs

---
+++ Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Flex_Shell
+{
+    private static class Math
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FLEX MATH VERSION 0.0.1");
+            Console.WriteLine();
+
+
+            Console.Write("FLEX$> ");
+            
+        }
+    }
+}
