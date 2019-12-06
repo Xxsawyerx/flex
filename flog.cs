655f0a90d9998fac4aa8d454e8d35ffc68e2d676
WindingTheRopes <ilikecomputers7@gmail.com>
2019-12-05 20:24:23 -0500

    Create flog.cs
    log erros and info with flog

---
+++ flog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class flog
+{
+	public static void e()
+    {
+        //log an error in flex-log.flog//
+    }
+    public static void i()
+    {
+        //log info in flex-log.flog//
+    }
+    
+}
